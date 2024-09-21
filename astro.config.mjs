49abcfc2406cc206403570d02392cee05de96787
Grewo <info@grew-development.de>
2024-09-21 15:40:16 +0200

    [Update] Startpage index.mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  site: process.env.CI
+    ? "https://wiki.grew-development.de"
+    : "http://localhost:4321",
+  base: process.env.CI ? "/wiki" : undefined,
+  integrations: [
+    starlight({
+      title: "Wiki - Grew Development",
+      favicon: "./src/assets/pics/favicon.ico",
+      social: {
+        github: "https://github.com/grew-development/wiki",
+        email: "mailto:info@grew-development.de",
+      },
+      customCss: ["./src/assets/css/main.css"],
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: "English",
+          lang: "en",
+        },
+        de: {
+          label: "Deutsch",
+          lang: "de",
+        },
+      },
+      sidebar: [
+        { label: 'Home', link: '/' },
+      ],
+    }),
+  ],
 });
